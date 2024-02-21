6309ba08a31336064debcc800c1983f64bad6b1a
Bao Nguyen <baon6052@gmail.com>
2024-02-21 17:17:52 +0000

    Add more sweep configs

--- main.py
+++ main.py
@@ -17,15 +17,21 @@ from models.gcn import GCN
 wandb_logger = WandbLogger(project="setup_tests", entity="r252_bel")
 
 
-def get_dataset(dataset_type: str, dataset_name: str, fold_idx: int):
+def get_dataset(
+    dataset_type: str, dataset_name: str, fold_idx: int, batch_size: int
+):
     if dataset_type == DatasetType.NODE_HOMO:
-        datamodule = PlanetoidDataset(PlanetoidDatasetType[dataset_name])
+        datamodule = PlanetoidDataset(
+            PlanetoidDatasetType[dataset_name], batch_size
+        )
     elif dataset_type == DatasetType.NODE_HETERO:
         datamodule = HeteroDataset(
             HeteroDatasetType[dataset_name], fold_idx=fold_idx
         )
     elif dataset_type == DatasetType.GRAPH_HOMO:
-        datamodule = GraphHomoDataset(GraphHomoDatasetType[dataset_name])
+        datamodule = GraphHomoDataset(
+            GraphHomoDatasetType[dataset_name], batch_size
+        )
     return datamodule
 
 
@@ -43,6 +49,8 @@ def get_dataset(dataset_type: str, dataset_name: str, fold_idx: int):
 @click.option("--num_layers", type=int, default=3)
 @click.option("--hidden_dim", type=int, default=64)
 @click.option("--graph_classification", type=bool, default=False)
+@click.option("--batch_size", type=int, default=64)
+@click.option("--with_sam", type=bool, default=True)
 @click.option("--seed", type=int, default=1234)
 def main(
     dataset_type: str,
@@ -51,15 +59,18 @@ def main(
     num_layers: int,
     hidden_dim: int,
     graph_classification: bool,
+    batch_size: int,
+    with_sam: bool,
     seed: int,
 ):
-    datamodule = get_dataset(dataset_type, dataset_name, fold_idx)
+    datamodule = get_dataset(dataset_type, dataset_name, fold_idx, batch_size)
     model = GCN(
         num_features=datamodule.num_features,
         num_classes=datamodule.num_classes,
         num_hidden=hidden_dim,
         num_hidden_layers=num_layers,
         graph_classification=graph_classification,
+        with_sam=with_sam,
         seed=seed,
     )
 
